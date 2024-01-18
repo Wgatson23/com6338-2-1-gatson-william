e605d85e73111ad7df279760e3940399f39ba3b9
William <wgatson@ufl.edu>
2024-01-17 20:11:29 -0500

    Wednesday

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // Your code here
 function greet(name) {
-    var name = prompt('What is your name?');
+	var name = prompt("What is your name?");
+	alert("Hello " + name);
 
-    prompt("How old are you?");
+	var age = prompt("How old are you?");
+	var birthday = confirm("Have you had a birthday yet this year?");
+
+	if (birthday) {
+		alert("You were born in " + (new Date().getFullYear() - age) + "!");
+	} else {
+		alert("You were born in " + (new Date().getFullYear() - age - 1) + "!");
+	}
 }
-greet(name);
